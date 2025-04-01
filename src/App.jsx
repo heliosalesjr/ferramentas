0d63a6311d8ccefc62afc3ff31b31b93f4140087
heliosalesjr <65938536+heliosalesjr@users.noreply.github.com>
2025-04-01 15:42:12 -0300

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,16 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <>
+      <h1 className='p-16 m-16 bg-pink-300 text-4xl text-center'>Hola hola holaaaaaa</h1>
+    </>
+  )
+}
+
+export default App
