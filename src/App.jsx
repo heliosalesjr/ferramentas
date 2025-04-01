cbcb059e549448b471d4a39a982dfb4550f8025f
heliosalesjr <65938536+heliosalesjr@users.noreply.github.com>
2025-04-01 16:04:31 -0300

    motion, shadcn & folder structure

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,14 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { Button } from "./components/ui/button"
+
 
 function App() {
-  const [count, setCount] = useState(0)
 
   return (
     <>
       <h1 className='p-16 m-16 bg-pink-300 text-4xl text-center'>Hola hola holaaaaaa</h1>
+      <Button className="m-16" variant="outline">
+        Me clique, menina!
+      </Button>
     </>
   )
 }
